b25f7bb40fe1a7e62fb4ef41c434d2a13d12b25b
Akshat Singh <144692981+Akshatcodesyou@users.noreply.github.com>
2024-09-02 15:05:00 +0400

    Create squarecube.java

---
+++ Lab-2/squarecube.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class squarecube {
+    public static void main(String[] args) {
+        System.out.println("\tNumber\tSquare\tCube");
+        for(int i=1; i<11; i++){
+            System.out.println(" \t"+ i + "\t" + i*i + "\t" + i*i*i);
+        }
+    }
+}
