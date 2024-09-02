c674c67e9022e88ae6325221d91b5c3382a9d59a
Akshat Singh <144692981+Akshatcodesyou@users.noreply.github.com>
2024-09-02 14:48:21 +0400

    Create wajp.java

---
+++ Lab-2/wajp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class wajp {
+    public static void main(String[] args) {
+        int num;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter a 5 digit number: ");
+        num = inp.nextInt();
+        System.out.println("The number is: "+num);
+        int num1 = 10000;
+        for(int i=0; i<5; i++){
+            System.out.println(" "+num/num1);
+            num = num%num1;
+            num1 = num1/10;
+        }
+    }
+}
