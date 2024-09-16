5ced7e90e4b8ef47b4356aedaebea12513141df6
Akshat Singh <144692981+Akshatcodesyou@users.noreply.github.com>
2024-09-16 14:45:38 +0400

    Update Element.java

--- Lab-3/Element.java
+++ Lab-3/Element.java
@@ -39,6 +39,55 @@ public class Element {
             System.out.println("Invalid input. Please enter 'y' or 'n'.");
         }
 
+
+
+
+// using method shown in class without directly making an array 
+import java.util.Scanner;
+
+public class Element {
+    int atnum;
+    String atname;
+    String atsymbol;
+    double atweight;
+
+    Element(int atnum, String atname, String atsymbol, double atweight) {
+        this.atnum = atnum;
+        this.atname = atname;
+        this.atsymbol = atsymbol;
+        this.atweight = atweight;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Element hydrogen = new Element(1, "Hydrogen", "H",1.008 );
+        Element scandium = new Element(1, "Scandium", "Sc", 44.9596);
+        Element chromium = new Element(24, "Chromium", "Cr", 51.9961);
+
+        Element[] elements = {hydrogen, scandium, chromium};
+
+        System.out.print("Only print elements above atomic weight 15? (y/n): ");
+        String input = scanner.nextLine().trim().toLowerCase();
+
+        if (input.equals("y")) {
+            for (int i = 0; i < elements.length; i++) {
+                if (elements[i].atweight > 15) {
+                    System.out.println("Element: " + elements[i].atname + " " + elements[i].atsymbol + " " + elements[i].atweight);
+                }
+            }
+        } else if (input.equals("n")) {
+            for (int i = 0; i < elements.length; i++) {
+                System.out.println("Element: " + elements[i].atname + " " + elements[i].atsymbol + " " + elements[i].atweight);
+            }
+        } else {
+            System.out.println("Invalid input. Please enter 'y' or 'n'.");
+        }
+
+        scanner.close();
+    }
+}
+
         scanner.close();
     }
 }
